beeb220d92f03fe9ab33c684f8fb81981a475ba8
Javier <javier@javier-HP-Compaq-6715b-KL428LA-ABM.(none)>
2011-01-15 11:04:55 -0600

    Inicio Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataBaseCompartir::Application.config.secret_token = '1764236f542aeb8b9b31ef4462e129530fae2cbd9decc17787aef798010fa9bff6be539052a179a08b471d1e4d98344978cd1543c2d220072a1d02de60c5a4c4'
